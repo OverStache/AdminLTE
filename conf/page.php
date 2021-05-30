cf653b68f361eff09b61cc5e59e1ac47883d79f7
OverStache <ivan.firmansyah1080@gmail.com>
2021-05-30 18:19:23 +0700

    setting nav-item class, conf>page.php

---
+++ conf/page.php
@@ -0,0 +1,16 @@
+<?php
+switch ($page) {
+    // beranda
+  case 'beranda':
+    include 'myPages/beranda.php';
+    break;
+  case 'beranda2';
+    include 'myPages/beranda2.php';
+    break;
+  case 'beranda3';
+    include 'myPages/beranda3.php';
+    break;
+  case 'table';
+    include 'myPages/table.php';
+    break;
+}
